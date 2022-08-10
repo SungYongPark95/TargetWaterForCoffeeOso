8e95da56ff38cb6841a36f28dd109ef8b2e2ae96
Hyeon-Jun-Kim <gomji81@naver.com>
2022-08-11 00:16:59 +0900

    [ AddDataTableViewCell : 데이터를 추가하는 화면(AddDataViewController)에서 중복 코드를 방지하기 위한 Custom Cell] 1. dataTypeLabel - 입력되는 데이터의 종류를 표시하기 위한 Label 2. textField - 데이터가 입력되는 TextField - 키보드 타입은 .numberpad 로 초기화

---
+++ TargetWaterForCoffee/AddDataTableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  AddDataTableViewCell.swift
+//  WaterForCoffee_Store
+//
+//  Created by 김현준 on 2022/08/08.
+//
+
+import UIKit
+
+class AddDataTableViewCell: UITableViewCell {
+    
+    let dataTypeLabel = UILabel()
+    let textField = UITextField()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(dataTypeLabel)
+        contentView.addSubview(textField)
+        
+        dataTypeLabel.translatesAutoresizingMaskIntoConstraints = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            dataTypeLabel.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor,constant: 16),
+            dataTypeLabel.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            dataTypeLabel.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            dataTypeLabel.widthAnchor.constraint(equalToConstant: 110),
+            
+            textField.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 16),
+            textField.leadingAnchor.constraint(equalTo: dataTypeLabel.trailingAnchor),
+            textField.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            textField.heightAnchor.constraint(equalToConstant: 22)
+        ])
+        
+        dataTypeLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        textField.font = UIFont.systemFont(ofSize: 15)
+        textField.keyboardType = .numberPad
+        textField.textAlignment = .right
+        textField.tintColor = .clear
+        
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
